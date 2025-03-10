4aa8d73009b836e07f78e25b41ffed3db0806f17
MUHAMMAD ZAKY AL-FARIZY <zakyfaryz@student.telkomuniversity.ac.id>
2025-03-10 22:50:11 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_103022330051/Program.cs
+++ tpmodul4_103022330051/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
- 
+using System;
+using System.Collections.Generic;
+
+class KodePos
+{
+    private static readonly Dictionary<string, string> kodePosMap = new Dictionary<string, string>
+    {
+        { "Batununggal", "40266" },
+        { "Kujangsari", "40287" },
+        { "Mengger", "40267" },
+        { "Wates", "40256" },
+        { "Cijaura", "40287" },
+        { "Jatisari", "40286" },
+        { "Margasari", "40286" },
+        { "Sekejati", "40286" },
+        { "Kebonwaru", "40272" },
+        { "Maleer", "40274" },
+        { "Samoja", "40273" }
+    };
+
+    public static string GetKodePos(string kelurahan)
+    {
+        return kodePosMap.TryGetValue(kelurahan, out string kodePos) ? kodePos : "Kode pos tidak ditemukan";
+    }
+}
